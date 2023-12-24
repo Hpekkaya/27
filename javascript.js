5ae2378ced78f87d645c9c6c5598aed588e30a48
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-24 21:00:32 +0300

    async and await

--- javascript.js
+++ javascript.js
@@ -45,5 +45,50 @@ console.log(myPromise2)})
 // 2.fulfilled
 // 3.rejected
 
+// async  functions
+
+// function myFunction() {
+//     return Promise.reject("Hello")    
+// }
+
+// async function myFunction2(){
+//     return "Hello async"
+// }
+// // Both above the same
+// myFunction().then(function(value){console.log("Resolve ", value)},
+// function(error){console.log("Reject ", error)})
+// myFunction2().then(function(value){console.log("Resolve ", value)},
+// function(error){console.log("Reject ", error)})
+
+// await  functions only can be used in async function
+// async function myDisplay(){
+//     let myPromise = new Promise(function(ressolve, reject){
+//         setTimeout(function(){ressolve("I love you")}, 1000)
+//     })
+//     // console.log(await myPromise)
+
+//     console.log(myPromise)
+//     await myPromise;
+//     console.log("Ended")
+// }
+// myDisplay()
+
+async function test(data){
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (data) {resolve("This is value")}
+            else {reject("This not value")}
+        }, 2000)
+    })
+    console.log("Promise pending")
+    let response = await promise;
+    console.log(response);
+    console.log("Promise ended");
+    return response
+}
+
+test("value").then (resolve => console.log(resolve), reject=> console.log(reject))
+// test(null).then (resolve => console.log(resolve), reject=> console.log(reject))
+
 
 
