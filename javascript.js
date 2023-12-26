8f38bdfb1044a937d520842c24d76bc43ae132e6
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-26 16:40:55 +0300

    continue of Lesson 27

--- javascript.js
+++ javascript.js
@@ -90,5 +90,40 @@ async function test(data){
 test("value").then (resolve => console.log(resolve), reject=> console.log(reject))
 // test(null).then (resolve => console.log(resolve), reject=> console.log(reject))
 
+// DOM Document Object Model
+// document.getElementById("demo").innerHTML = "Hello World"
+// console.log(document.getElementsByTagName("p"))
 
+// document.getElementsByTagName("p")[0].innerHTML = " Hello TagName 0";
+// document.getElementsByTagName("p")[1].innerHTML = " Hello TagName 1";
+
+// Above the same
+
+// for (let i in document.getElementsByTagName("p")) {
+//   (document.getElementsByTagName("p")[i].innerHTML = "Hello TagName " + i)
+// }
+
+// document.getElementsByClassName("intro")[0].innerHTML= " Hello Class Name " + 0;
+
+// If we want to use querySelectorAll
+// we should use . for class  -  # for id's
+// p element and into class
+// document.querySelectorAll("p.intro")[0].innerHTML = " Hello : p element and intro claas" + 0 
+
+// document.getElementById("image").src = "smiley.gif"
+// let imageDOM = document.getElementById("image")
+// imageDOM.src = "smiley.gif"; imageDOM.width="100"; imageDOM.height="100"
+
+// function validateForm() {
+// //   let x = document.forms["myForm"]["fname"].value;
+// //   if (x == "") {
+// //     document.querySelectorAll("#alert")[0].innerHTML ="Name must be filled out"
+// //     // alert("Name must be filled out");
+// //     return false;
+// //   }
+// // }
+
+// document.getElementById("demo").style.color ="red"
+// document.getElementById("demo2").style.backgroundColor="yellow"
+// console.log(document.getElementById("demo2"))
 
