e04f6ac8ec6a0431fdff24ce8b89be0c294cf104
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-26 20:15:40 +0300

    onchange

--- javascript.js
+++ javascript.js
@@ -127,5 +127,30 @@ test("value").then (resolve => console.log(resolve), reject=> console.log(reject
 // document.getElementById("demo2").style.backgroundColor="yellow"
 // console.log(document.getElementById("demo2"))
 
+// Using events
+// function changeColor(){
+//     let x = document.getElementById("id1")
+//     x.style.color = "red"
+// }
+
+// function hide(){
+//     let x = document.getElementById("p2")
+//     x.style.visibility = "hidden"
+// }
+//  let hide = () => {
+//     let x = document.getElementById("p2")
+//     x.style.visibility = "hidden"
+// }
+// function show(){
+//     let x = document.getElementById("p2")
+//     x.style.visibility = "visible"
+// }
+
+// let changeText = () => {
+//     let x = document.getElementById("p3")
+//     x.innerHTML = 'Ooooops!'
+// }   
+// let changeText = (id) => id.innerHTML = 'Ooooops!'
+
 
 
--- trial.js
+++ trial.js
@@ -1,26 +1,5 @@
-// Using events
-function changeColor(){
-    let x = document.getElementById("id1")
-    x.style.color = "red"
+upperCase = () => {
+    const x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
 }
-
-// function hide(){
-//     let x = document.getElementById("p2")
-//     x.style.visibility = "hidden"
-// }
- let hide = () => {
-    let x = document.getElementById("p2")
-    x.style.visibility = "hidden"
-}
-function show(){
-    let x = document.getElementById("p2")
-    x.style.visibility = "visible"
-}
-
-// let changeText = () => {
-//     let x = document.getElementById("p3")
-//     x.innerHTML = 'Ooooops!'
-// }   
-let changeText = (id) => id.innerHTML = 'Ooooops!'
-   
     
