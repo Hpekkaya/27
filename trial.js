9f5e144287a78630b745181ae8e0fecf25389891
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-26 21:16:11 +0300

    addEventListener tried

--- trial.js
+++ trial.js
@@ -2,4 +2,18 @@ upperCase = () => {
     const x = document.getElementById("fname");
     x.value = x.value.toUpperCase();
 }
+
+mOver = (obj) => {
+    obj.innerHTML = "Thanks"
+    obj.style.backgroundColor = "blue"
+    obj.style.color = "white"
     
+}
+mOut = (obj) => {
+    obj.innerHTML = "Mouse Over Me" 
+    obj.style.backgroundColor = "red"
+}
+
+document.getElementById("myBtn").addEventListener("click", myAlert = () => alert("Hello World"))
+
+
