7af1a749585a6c3847e154756b4e2f9998f674c0
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-27 12:09:01 +0300

    removeHandler

--- trial.js
+++ trial.js
@@ -1,26 +1,32 @@
 
-document.getElementById("myP1").addEventListener("click",
-function(){
-    alert("myP1")
-},false
-)
-document.getElementById("myDiv1").addEventListener(
-    "click",
-    function(){
-        alert("myDiv1")
-    },false
-)
-document.getElementById("myP2").addEventListener("click",
-function(){
-    alert("myP2")
-},true
-)
-document.getElementById("myDiv2").addEventListener(
-    "click",
-    function(){
-        alert("myDiv2")
-    },true
-)
+// document.getElementById("myP1").addEventListener("click",
+// function(){
+//     alert("myP1")
+// },false
+// )
+// document.getElementById("myDiv1").addEventListener(
+//     "click",
+//     function(){
+//         alert("myDiv1")
+//     },false
+// )
+// document.getElementById("myP2").addEventListener("click",
+// function(){
+//     alert("myP2")
+// },true
+// )
+// document.getElementById("myDiv2").addEventListener(
+//     "click",
+//     function(){
+//         alert("myDiv2")
+//     },true
+// )
 
-
+document.getElementById("myDIV").addEventListener("mousemove",myFunction)
+function myFunction(){
+    document.getElementById("demo").innerHTML=Math.random()
+}
+function removeHandler(){
+    document.getElementById("myDIV").removeEventListener("mousemove",myFunction)
+}
 
